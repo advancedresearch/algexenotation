c5f4ba32b1652d9969b1c5f7bddf90c6db5488ec
Sven Nilsen <bvssvni@gmail.com>
2022-09-17 12:53:02 +0200

    Improve performance for some kinds of expressions
    Improves evaluation performance for expressions that are composed of multiplication and power only.

---
+++ examples/bench.rs
@@ -0,0 +1,16 @@
+use algexenotation::Algexeno;
+
+fn main() {
+    let mut x = 0;
+    let n = 200_000;
+    let mut c = 0;
+    loop {
+        let y = Algexeno::Orig(x).eval();
+        if let Algexeno::Const(_) = y {
+            c += 1;
+        }
+        x += 1;
+        if x > n {break}
+    }
+    println!("{}", c);
+}
