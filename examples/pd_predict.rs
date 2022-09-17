f73b9d95a4deac9cb101ad6d86ba4d4f41320f36
Sven Nilsen <bvssvni@gmail.com>
2022-09-17 12:48:12 +0200

    Added examples predicting patterns

---
+++ examples/pd_predict.rs
@@ -0,0 +1,32 @@
+/*
+
+Shows that there is a predictable pattern in `pd`.
+
+*/
+
+use algexenotation::*;
+
+fn main() {
+    let mut x = 2;
+    let n = std::u64::MAX;
+    let mut count = 0;
+    let mut last = 1;
+    loop {
+        let mut m = 0;
+        let mut y = x;
+        while y != 2 {
+            y = y / pd(y) + 1;
+            m += 1;
+        }
+        if m > count {
+            println!("{}' ({})", x, m);
+            count = m;
+            println!("  {}", x as f64 / last as f64);
+            last = x;
+            x = x * 2 - 2;
+            if x < last {break}
+        }
+        x += 1;
+        if x >= n {break}
+    }
+}
