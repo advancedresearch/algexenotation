bc161cf5fa172000b357d6902d57b2414f93108c
Sven Nilsen <bvssvni@gmail.com>
2025-01-25 11:38:24 +0100

    Updated "primbix" example
    - Added support for multi-threaded code

--- examples/primbix.rs
+++ examples/primbix.rs
@@ -29,23 +29,49 @@ Uses a single thread.
 use algexenotation::primbix;
 
 fn main() {
-    let n: u64 = 1_000_000_000_000_000;
-    let mut depth = 14;
-    let mut x = 25_000_000_000;
-    loop {
-        if x >= n {break}
+    let start = 0;
+    let end = start + 10000;
+    let threads = 2;
+    let res = min_primbix(start, end, threads);
+    println!("\n[{}, {}): {}, {}", start, end, res.0, res.1);
+    println!("END");
+}
 
-        let y = primbix(x);
-        if y > depth {
-            println!("{}: {}", x, y);
-            depth = y;
+/// Calculates the minimum highest primbix found in range, using a specified number of threads.
+///
+/// Should be called starting with an even number.
+pub fn min_primbix(start: u64, end: u64, threads: u8) -> (u64, u64) {
+    fn f(start: u64, end: u64, id: u8) -> (u64, u64) {
+        println!("{}: [{}, {})", id, start, end);
+        let mut m = (0, start);
+        for x in start..end {
+            if x % 2 == 0 {continue}
+            if x % 3 == 0 {continue}
+            if x % 5 == 0 {continue}
 
-            // I think it is safe to double.
-            x *= 2;
-        } else if x % 1_000_000 == 0 {
-            println!("{}", x);
+            let y = primbix(x);
+            if y > m.0 {
+                println!("{}: {}, {}", id, y, x);
+                m = (y, x)
+            }
         }
-
-        x += 1;
+        m
     }
+
+    let mut s = start;
+    let chunk = (end - start) / threads as u64;
+
+    let mut handles = vec![];
+    for th in 0..threads {
+        let size = if th + 1 == threads {end - s} else {chunk};
+        handles.push(std::thread::spawn(move || f(s, s + size, th)));
+        s += size;
+    }
+
+    let mut m = (0, start);
+    for h in handles.into_iter() {
+        let res = h.join().unwrap();
+        if res.0 > m.0 || (res.0 == m.0 && res.1 < m.1) {m = res}
+    }
+    m
 }
